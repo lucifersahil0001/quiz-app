d7daf6f76bf119efe70685b92534f35757405ad3
lucifersahil0001 <harshgupta00010@gmail.com>
2023-05-19 10:16:09 +0530

    Quiz-app

---
+++ src/Data/QuizData.jsx
@@ -0,0 +1,42 @@
+export const QuizData = [
+    {
+        question: "Self-Management skills refer to?",
+        options:[ "always keep busy with oneself", "live life with a positive attitude", "controls the anger", "motivating oneself and setting goals"],
+        answer: 4
+    },
+    {
+        question: "Ms Hiral started a career as a fresher in an IT Company. She wants to perform well at work and in life in general. Which of the following skills does she need to improve??",
+        options:["Discipline", "Timeliness", "professionalism", "All of the above"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "Mr. Bhupesh did a project very well. Now he is collecting feedback, and insights of his work-specific proficiencies. This is called:?",
+        options: ["Self-Awareness", "Responsibility", "Time-Management", "Adaptability"],
+        answer:1
+    },
+    {
+        question:  "Smit has prepared a timetable to achieve his goal. This skill is known as",
+        options: ["Self-Awareness", "  Responsibility, ", "Time-Management", "Adaptability"],
+        answer:3
+    },
+    {
+        question: "What is BOM?",
+        options: [" BOM stands for Browser  of Modle. ", "   BOM stands for Browser Old Model. ", "BOM stands for Browser  Over Model. ", " BOM stands for Browser Object Model. "],
+        answer:4
+    },
+    {
+        question: "  How to create objects in JavaScript?",
+        options: ["  {} ", "  () ", " // ", "  [[]] "],
+        answer:1
+    },
+    {
+        question: "  What are the pop-up boxes available in JavaScript?",
+        options: ["  Prompt Box ", "  Confirm Box ", "  Alert Box ", " none of above "],
+        answer:3
+    },
+];
